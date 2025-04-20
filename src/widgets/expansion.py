d45e6f50e8ade30c63394305c77b1b5e8b5ac31e
iluminate1 <bitbuk@gmail.com>
2025-04-21 01:00:10 +0300

    feat: implement source image load

--- src/widgets/expansion.py
+++ src/widgets/expansion.py
@@ -1,9 +1,52 @@
-from PySide6.QtWidgets import QWidget
+from typing import override
+
+from PySide6.QtGui import QImage, QResizeEvent
+from PySide6.QtWidgets import QMessageBox, QWidget
 
 from ui import ExpansionWidgetUI
+from utils import ImageFormat, load_image, set_pixmap
+from utils.exceptions import (
+    ImageLoadException,
+    InvalidFileException,
+    InvalidImageFormat,
+)
 
 
 class ExpansionWidget(QWidget, ExpansionWidgetUI):
     def __init__(self, parent: QWidget | None = None):
         super().__init__(parent=parent)
         self.setupUi(self)
+
+        self.source_image: QImage
+        self.expanded_image: QImage
+
+        self.connect_signals()
+
+    @override
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        if not hasattr(self, "source_image"):
+            return
+
+        set_pixmap(self.source_image, self.source_image_label_pixmap)
+        set_pixmap(self.expanded_image, self.expanded_image_label_pixmap)
+
+    def connect_signals(self) -> None:
+        _ = self.load_source_image_button.clicked.connect(self.load_source_image)
+
+    def load_source_image(self) -> None:
+        try:
+            img = load_image(self, ImageFormat.binary)
+        except InvalidFileException:
+            return
+        except ImageLoadException as e:
+            _ = QMessageBox.warning(self, e.msg, e.msg)
+            return
+        except InvalidImageFormat as e:
+            _ = QMessageBox.warning(self, str(e.msg), "Image is not binary!!!")
+            return
+        else:
+            self.source_image = img
+
+        set_pixmap(img, self.source_image_label_pixmap)
+        self.expanded_image = img.copy()
+        set_pixmap(self.expanded_image, self.expanded_image_label_pixmap)
