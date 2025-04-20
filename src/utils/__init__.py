a6f67d6d9beebe462761b72d89c6355bbf341a8a
iluminate1 <bitbuk@gmail.com>
2025-04-21 00:59:09 +0300

    refactor: add set_pixmap to utils package export

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,10 +6,11 @@ __all__ = (
     "inverse_pixel",
     "load_image",
     "save_image",
+    "set_pixmap",
     "sum_of_surrounding_pixels",
     "to_binary_format",
 )
 from .exceptions import ImageLoadException, InvalidFileException, InvalidImageFormat
-from .image_helper import ImageFormat, load_image, save_image
+from .image_helper import ImageFormat, load_image, save_image, set_pixmap
 from .pixels_helper import inverse_pixel, to_binary_format
 from .sum_of_surrounding_pixels import sum_of_surrounding_pixels
